e964a4cfa75b293391b658f911c7c46975e7bae2
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-10-25 07:02:12 +0100

    added user uploading post view and functionality

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
+    path('upload', views.upload, name='upload'),
     path('settings', views.settings, name='settings'),
 ]
